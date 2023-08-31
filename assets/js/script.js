e65cb080f1e135de23c62498aa6c3c795e2c2ef8
VJacobo <victoriajacobo99@gmail.com>
2023-08-31 08:24:38 -0500

    started with html, css and javascript code

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById('startButton');
+const outputText = document.getElementById('outputText');
+
+startButton.addEventListener('click', () => {
+    outputText.textContent = 'Thanks for clicking! c:';
+});
