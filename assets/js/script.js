e27f00d2f85692e5e6d6e306a73e9154644d15bf
VJacobo <victoriajacobo99@gmail.com>
2023-09-02 14:40:24 -0500

    Revised my css.html, and JavaScript. It is simplified and easier to read.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-const startButton = document.getElementById('startButton');
-const outputText = document.getElementById('outputText');
-
-startButton.addEventListener('click', () => {
-    outputText.textContent = 'Thanks for clicking! c:';
-});
+const questions = [
+    {
+        question: ""
+    }
+]
