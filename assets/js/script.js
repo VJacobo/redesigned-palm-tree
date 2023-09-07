484259197f4ca968fe792b419b4005065536bb58
VJacobo <victoriajacobo99@gmail.com>
2023-09-07 16:27:49 -0500

    added better user accessability

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,59 +2,105 @@ const questions = [
     {
         question: "Commonly used data types do NOT include:",
         answers: [
-            { text: "strings", correct: false},
-            { text: "booleans", correct: false},
-            { text: "alerts", correct: false},
-            { text: "numbers", correct: true},
-        ]
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "alerts", correct: false },
+            { text: "numbers", correct: true },
+        ],
     },
     {
         question: "The condition in an if / else statement is enclosed with ____",
         answers: [
-            { text: "quotes", correct: false},
-            { text: "curly brackts", correct: false},
-            { text: "parenthesis", correct: true},
-            { text: "square brackets", correct: false},
-        ]  
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "square brackets", correct: false },
+        ],
     },
     {
         question: "Arrays in JavaScript can be used to store ____.",
         answers: [
-            { text: "numbers and strings", correct: false},
-            { text: "other arrays", correct: false},
-            { text: "parenthesis", correct: true},
-            { text: "square brackets", correct: false},
-        ]  
+            { text: "numbers and strings", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "functions", correct: false },
+            { text: "all of the above", correct: true },
+        ],
     },
-]
+];
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("answer-btn");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
-    let currentQuestion = question[currentQuestionIndex];
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    answerButton.innerHTML = "";
-
-    currentQuestion.answers.forEach(answers => {
+    currentQuestion.answers.forEach((answer) => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
+    
     });
 }
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block"
+}
+
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        endQuiz();
+    }
+});
+
+function endQuiz() {
+    questionElement.innerHTML = "Quiz Over! Your Score: " + score + "/" + questions.length;
+    nextButton.style.display = "none";
+    
+    resetState();
+}
+
 
 startQuiz();
